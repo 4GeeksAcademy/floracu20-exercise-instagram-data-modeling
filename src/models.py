5ec48b05b90e94007a0362e4df40d52bf104ce75
Florencia Acuña <110423383+floracu20@users.noreply.github.com>
2024-11-12 02:28:25 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+""" class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -26,7 +26,42 @@ class Address(Base):
     person = relationship(Person)
 
     def to_dict(self):
-        return {}
+        return {} """
+
+
+class User(Base):
+    __tablename__='user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), nullable=False, unique=True)
+    firstname = Column(String(30))
+    lastname = Column(String(30))
+    email = Column(String(40), nullable=False, unique=True)
+    comments = relationship('Comment', back_populates="author_relationship")
+    user_from_ids = relationship('Follower', back_populates="user_from_id_relationship")
+    user_to_ids = relationship('Follower', back_populates="user_to_id_relationship")
+
+class Follower(Base):
+    __tablename__='follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_from_id_relationship = relationship('User', back_populates="user_from_ids")
+
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id_relationship = relationship('User', back_populates="user_to_ids")    
+
+class Comment(Base):
+    __tablename__='comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(200), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    author_relationship = relationship('User', back_populates="comments")
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post_id_relationship = relationship('Post', back_populates="id_posts")
+
+class Post(Base):
+    __tablename__='post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    id_posts = relationship('Comment', back_populates="post_id_relationship")
 
 ## Draw from SQLAlchemy base
 try:
