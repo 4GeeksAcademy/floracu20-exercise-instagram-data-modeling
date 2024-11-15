3b3f44820104670c14e980b5c71f861a1d74978a
Florencia Acuña <110423383+floracu20@users.noreply.github.com>
2024-11-15 22:50:27 +0000

    Correcciones en clases comment y post

--- src/models.py
+++ src/models.py
@@ -7,28 +7,6 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-""" class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {} """
-
-
 class User(Base):
     __tablename__='user'
     id = Column(Integer, primary_key=True)
@@ -36,17 +14,17 @@ class User(Base):
     firstname = Column(String(30))
     lastname = Column(String(30))
     email = Column(String(40), nullable=False, unique=True)
-    comments = relationship('Comment', back_populates="author_relationship")
-    user_from_ids = relationship('Follower', back_populates="user_from_id_relationship")
-    user_to_ids = relationship('Follower', back_populates="user_to_id_relationship")
+    comments = relationship('comment', back_populates="author_relationship")
+    followers = relationship('Follower', back_populates="followed_user")
+    following = relationship('Follower', back_populates="follower_user")
 
 class Follower(Base):
     __tablename__='follower'
     user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    user_from_id_relationship = relationship('User', back_populates="user_from_ids")
+    follower_user = relationship('User', back_populates = 'following')
 
     user_to_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id_relationship = relationship('User', back_populates="user_to_ids")    
+    followed_user = relationship('User', back_populates = 'followers')    
 
 class Comment(Base):
     __tablename__='comment'
@@ -55,16 +33,16 @@ class Comment(Base):
     author_id = Column(Integer, ForeignKey('user.id'))
     author_relationship = relationship('User', back_populates="comments")
     post_id = Column(Integer, ForeignKey('post.id'))
-    post_id_relationship = relationship('Post', back_populates="id_posts")
+    post = relationship('Post', back_populates="comments")
 
 class Post(Base):
     __tablename__='post'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer)
-    id_posts = relationship('Comment', back_populates="post_id_relationship")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comments = relationship('Comment', back_populates="post")
 
 ## Draw from SQLAlchemy base
-try:
+try: 
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
